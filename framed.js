dede65321ee8dec6409cd506f2aba9d08bca6092
Owen Smith <owen@omsmith.ca>
2019-07-08 09:46:12 -0400

    deps: ifrau@0.27.0. use slim client (#26)

--- framed.js
+++ framed.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var Client = require('ifrau/client'),
+var Client = require('ifrau/client/slim'),
 	Promise = require('lie');
 
 var REQUEST_KEY = require('./request-key');
@@ -9,12 +9,7 @@ module.exports = function getFramedJwt(scope) {
 	return Promise
 		.resolve()
 		.then(function() {
-			return new Client({
-				resizeFrame: false,
-				syncTitle: false,
-				syncLang: false,
-				syncFont: false
-			}).connect();
+			return new Client().connect();
 		})
 		.then(function(client) {
 			return client.request(REQUEST_KEY, scope);
